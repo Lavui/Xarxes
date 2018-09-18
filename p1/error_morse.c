5c6cb02b91f85bea187eef8c3ac3f7c1d117ea1a
Lavui <eloipoi@gmail.com>
2018-09-18 11:46:59 +0200

    començat modul add_check

--- p1/error_morse.c
+++ p1/error_morse.c
@@ -1,9 +1,22 @@
+#include "error_morse.h"
 
 
-
-
-
-char add_check(char p[]){
+check add_check(char p[]){
+  uint16_t suma,carry,rest;
+  uint8_t che_bin;
+  check che_hex;
   
+  for (uint8_t i=0; p[i]; i++){ // funcio sumadora checksum
+    if ((p[i]>='A' && p[i]<='Z') || (p[i]>='0' && p[i]<='9') || (p[i]==' ')){
+      suma+=p[i];
+    }
+  }
+  rest = suma & 0x00FF;
+  carry = suma & 0xFF00;
+  carry = carry >> 8;
+  che_bin = ~(carry + rest); // checksum en binari
+
+  che_hex=byte2hex(che_bin);
+  return che_hex;
 }
 
--- p1/error_morse.h
+++ p1/error_morse.h
@@ -1,11 +1,20 @@
 #ifndef ERROR_MORSE_H
 #define ERROR_MORSE_H
+
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
 
-char add_check(char p[]);//Entrada: un string el qual se l'hi creara un Checksum d'un Byte. Sortida: Retorna el mateix string amb el Checksum (en hexa) al final. També retorna especificament el Checksum.
+
+typedef struct lh{
+  uint8_t L,H;
+}check;
+
+check add_check(char p[]);//Entrada: un string el qual se l'hi creara un Checksum d'un Byte. Sortida: Retorna el mateix string amb el Checksum (en hexa) al final. També retorna especificament el Checksum.
 
 bool check_is_ok(char p[]);//Entrada: String + el Checksum corresponent. Sortida: Bolea. True -> no error, False -> error.
 
-char get_redun(char p[]);//Entrada: String + checksum. Sortida: String sense Checksum i Checksum sense string.
+check get_redun(char p[]);//Entrada: String + checksum. Sortida: String sense Checksum i Checksum sense string.
+
+
+#endif
