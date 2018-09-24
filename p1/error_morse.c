482315ad0587989b1839870dfc30d8031f776485
Lavui <eloipoi@gmail.com>
2018-09-24 11:48:39 +0200

    funcions aux afegides i funcionant

--- p1/error_morse.c
+++ p1/error_morse.c
@@ -1,4 +1,5 @@
 #include "error_morse.h"
+#include <stdio.h>//Pel joc de proves (borrar al acabar)
 
 
 check add_check(char p[]){
@@ -20,3 +21,56 @@ check add_check(char p[]){
   return che_hex;
 }
 
+
+static check byte2hex(uint8_t byte){
+  check hex;
+  uint8_t H=byte>>4;
+  uint8_t L=byte & 0b00001111;
+  if(H>=0x0A && H<=0x0F)
+    H+=55;
+  else
+    H+=48;
+
+  if(L>=0x0A && L<=0x0F)
+    L+=55;
+  else
+    L+=48;
+  //printf("%c %c\n",H,L);
+  hex.H=H;
+  hex.L=L;
+  return hex;
+}
+
+
+static uint8_t hex2byte(check hex){
+  uint8_t maj, men;
+  if (hex.H>=0x41 && hex.H<=0x5A)
+    hex.H-=55;
+  else
+    hex.H-=48;
+
+  if (hex.L>=0x41 && hex.L<=0x5A)
+    hex.L-=55;
+  else
+    hex.L-=48;
+  //printf("%i L \n",hex.L);
+
+  maj=hex.H<<4;
+  men=hex.L & 0b00001111;
+  //printf("%i maj | %i men = %i\n",maj,men,maj|men);
+
+  return maj|men;
+}
+
+
+/*
+void main(){
+  check hexad;
+  //hexad.L=0x0F;
+  //hexad.H=0x00;
+  //printf("%i\n",hex2byte(hexad)); // prova hex2byte
+
+  uint8_t byt= 0b01010101;
+  
+  printf("%i\n",hex2byte(byte2hex(byt))); // prova byte2hex
+  }*/
--- p1/error_morse.h
+++ p1/error_morse.h
@@ -10,11 +10,20 @@ typedef struct lh{
   uint8_t L,H;
 }check;
 
+
 check add_check(char p[]);//Entrada: un string el qual se l'hi creara un Checksum d'un Byte. Sortida: Retorna el mateix string amb el Checksum (en hexa) al final. També retorna especificament el Checksum.
 
 bool check_is_ok(char p[]);//Entrada: String + el Checksum corresponent. Sortida: Bolea. True -> no error, False -> error.
 
 check get_redun(char p[]);//Entrada: String + checksum. Sortida: String sense Checksum i Checksum sense string.
+
+
+
+/*funcions auxiliars*/
+
+static check byte2hex(uint8_t byte);
+static uint8_t hex2byte(check hex);
+
 
 
 #endif
