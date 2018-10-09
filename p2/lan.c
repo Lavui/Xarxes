7ec2f65d0ccacc546a84136ff46c0510d22162c5
David Terés <david.teres@hotmail.com>
2018-10-09 10:52:53 +0200

    p2

---
+++ p2/lan.c
@@ -0,0 +1,5 @@
+#include "lan.h"
+#include "error_morse.h"
+#include <pbn.h>
+#include <stdlib.h>
+#include <avr/io.h>
---
+++ p2/lan.h
@@ -0,0 +1,21 @@
+#ifndef LAN_H
+#define LAN_H
+#include <inttypes.h>
+#include <stdbool.h>
+
+typedef void (*lan_callback_t)(void);
+typedef uint8_t * missatge_lan_t;
+
+void lan_init(uint8_t no);
+
+bool lan_can_put(void);
+
+void lan_block_put(const missatge_lan_t m, uint8_t nd);
+
+//bool lan_can_get(void);
+
+uint8_t lan_block_get(missatge_lan_t m);
+
+void on_lan_received(lan_callback_t l);
+
+#endif
