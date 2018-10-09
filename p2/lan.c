9554063ac78f1f9ce24dbf994e2af771fb4318bd
David Terés <david.teres@hotmail.com>
2018-10-09 11:47:30 +0200

    p2 comensada

--- p2/lan.c
+++ p2/lan.c
@@ -3,3 +3,12 @@
 #include <pbn.h>
 #include <stdlib.h>
 #include <avr/io.h>
+
+static void envia_trama(void); //uniques d'aqui
+static void rep_trama(void);
+
+typedef struct {
+  uint8_t origen;
+  uint8_t desti;
+  uint8_t payload;
+}
