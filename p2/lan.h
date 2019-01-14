666cb6a4b4e8e5e31f5946d04f7c1a96f14f9b15
Lavui <eloipoi@gmail.com>
2019-01-14 11:29:16 +0100

    .h modificat

--- p2/lan.h
+++ p2/lan.h
@@ -18,14 +18,14 @@ typedef enum{
 typedef void (*lan_callback_t)(void);
 typedef uint8_t * missatge_lan_t;
 
-void lan_init(uint8_t no);
+void lan_init(uint8_t no);//inicialitza el mòdul i per tant el protocol. Té com paràmetre "no" que és l'adreça origen del propi node.
 
-bool lan_can_put(lan_buffer_t prioritat);
+bool lan_can_put(lan_buffer_t prioritat);//gestiona la  predefinida sobre la transmisió pel canal. Retornara true si la H o la L del buffer estan lliures i retorna false en cas contrari.???????????
 
-void lan_block_put(const missatge_lan_t m, uint8_t nd, lan_buffer_t prioritat);
+void lan_block_put(const missatge_lan_t m, uint8_t nd, lan_buffer_t prioritat);//té dos paràmetres. El primer és el missatge que es vol transmetre en forma de taula missatge_lan_t . El segon és l'adreça del node a qui va dirigit.
 
-uint8_t lan_block_get(missatge_lan_t m);
+uint8_t lan_block_get(missatge_lan_t m);//té com a paràmetre la taula missatge_lan_t on es recollirà el missatge. També retorna l'adreça del node que ha enviat el missatge. Aquesta funció està molt relacionada amb lan_can_get() ja que aquesta última indica quan hi ha disponible un missatge per llegir i per tant quan es pot cridar a lan_block_get().
 
-void on_lan_received(lan_callback_t l);
+void on_lan_received(lan_callback_t l);//fa un callback de la funció que te al parametre "l".
 
 #endif
