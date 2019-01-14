06a2e6d34c72b8e9fb6b9ac0472f5dcd457ecb51
David Terés <david.teres@hotmail.com>
2019-01-14 10:06:52 +0100

    .h

--- p2/lan.h
+++ p2/lan.h
@@ -1,10 +1,8 @@
 #ifndef LAN_H
 #define LAN_H
+
 #include <inttypes.h>
 #include <stdbool.h>
-
-typedef void (*lan_callback_t)(void);
-typedef uint8_t * missatge_lan_t;
 
 typedef struct {
   uint8_t origen;
@@ -12,13 +10,19 @@ typedef struct {
   uint8_t payload[30];
 } lanpdu_t;
 
+typedef enum{
+  high,
+  low
+}lan_buffer_t;
 
+typedef void (*lan_callback_t)(void);
+typedef uint8_t * missatge_lan_t;
 
 void lan_init(uint8_t no);
 
-bool lan_can_put(void);
+bool lan_can_put(lan_buffer_t prioritat);
 
-void lan_block_put(const missatge_lan_t m, uint8_t nd);
+void lan_block_put(const missatge_lan_t m, uint8_t nd, lan_buffer_t prioritat);
 
 uint8_t lan_block_get(missatge_lan_t m);
 
