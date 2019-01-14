8c67695e03321fe0438b4d8622734c53c25f33ae
David Terés <david.teres@hotmail.com>
2019-01-14 11:28:27 +0100

    .c

--- p2/lan.c
+++ p2/lan.c
@@ -1,11 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pbn.h>
 #include "lan.h"
 #include "error_morse.h"
-#include <pbn.h>
-#include <stdlib.h>
-#include <avr/io.h>
 
-static void envia_trama(void); //uniques d'aqui
-static void rep_trama(void);
-static void reintents(void);
+static queue_t H,L;
 
+static uint8_t node_origen;
 
+static missatge_lan_t missatge;
+
+static lan_callback_t lan_cb=NULL;
+
+void lan_init(uint8_t no){
+  ether_init();
+  timer_init();
+  on_message_received(message_for_me);
+  node_origen=no;
+  queue_empty(&H);
+  queue_empty(&L);
+  timer_every(TIMER_MS(5000),checking_queue)
+}
