3862cb01237add01fdd94846b64a8023c8b38994
David Terés <david.teres@hotmail.com>
2019-01-16 21:47:23 +0100

    new queue

---
+++ p2/queue_t.h
@@ -0,0 +1,30 @@
+#ifndef QUEUE_T_H_
+#define QUEUE_T_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "lan.h"
+
+#define MAXCUA 10
+
+typedef struct {
+  int front;
+  int rear;
+  int count;
+  lan_pdu_t elements[MAXCUA];
+}queue_t;
+
+void queue_empty(queue_t *const q);
+
+bool queue_is_empty(const queue_t *const q);
+
+bool queue_is_full(const queue_t *const q);
+
+void queue_enqueue(queue_t *const q, lan_pdu_t v);
+
+void queue_dequeue(queue_t *const q);
+
+void queue_front(const queue_t *const q, lan_pdu_t *a);
+
+#endif
